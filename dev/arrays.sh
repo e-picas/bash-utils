92d7b53cb0d28fabf2d30a3ad275bd9226a50efc
piwi <me@e-piwi.fr>
2015-08-19 00:19:19 +0200

    review of the printer tester

--- dev/arrays.sh
+++ dev/arrays.sh
@@ -14,7 +14,7 @@ use print-list
 
 
 ######################################################
-echo "> demo of a table-like printing:"
+echo '############ DEMO OF print-table:'
 echo
 
 # declare the table array
@@ -24,22 +24,22 @@ declare -a TABLE_ITEMS=()
 # item [0] is padder for separation lines cells
 # item [1] is padder for contents lines cells
 declare -a TABLE_PADDERS=()
-TABLE_PADDERS[0]="$(printf '%0.1s' "-"{1..120})"
-TABLE_PADDERS[1]="$(printf '%0.1s' " "{1..120})"
+TABLE_PADDERS[0]="$(printf '%0.1s' "#"{1..120})"
+TABLE_PADDERS[1]="$(printf '%0.1s' "_"{1..120})"
 
 # declare the table paddings (number of space(s) from line to cell's contents)
 # item [0] is padding for separation lines cells
 # item [1] is padding for contents lines cells
 declare -a TABLE_PADDINGS=()
-TABLE_PADDINGS[0]=0
-TABLE_PADDINGS[1]=1
+TABLE_PADDINGS[0]=2
+TABLE_PADDINGS[1]=3
 
 # declare the table separators (characters used to separate cells)
 # item [0] is the separator for separation lines
 # item [1] is the separator for contents lines
 declare -a TABLE_SEPARATORS=()
-TABLE_SEPARATORS[0]='+'
-TABLE_SEPARATORS[1]='|'
+TABLE_SEPARATORS[0]='/'
+TABLE_SEPARATORS[1]='-'
 
 # first item are headers
 declare -a TABLE_HEADERS=( "name" "value" "comment" )
@@ -53,41 +53,42 @@ TABLE_ITEMS[${#TABLE_ITEMS[@]}]=SUB_0[@]
 declare -a SUB_1=("my name2" "lorem ipsum dolor sit amet")
 TABLE_ITEMS[${#TABLE_ITEMS[@]}]=SUB_1[@]
 
-echo
 echo '###########################'
+echo '> full custom:'
 echo
 print_table TABLE_ITEMS[@] TABLE_HEADERS[@] TABLE_PADDINGS[@] TABLE_PADDERS[@] TABLE_SEPARATORS[@]
 
 echo
 echo '###########################'
+echo '> with default separators:'
 echo
 print_table TABLE_ITEMS[@] TABLE_HEADERS[@] TABLE_PADDINGS[@] TABLE_PADDERS[@]
 
 echo
 echo '###########################'
+echo '> with default separators & padders:'
 echo
 print_table TABLE_ITEMS[@] TABLE_HEADERS[@] TABLE_PADDINGS[@]
 
 echo
 echo '###########################'
+echo '> with default separators & padders & paddings:'
 echo
 print_table TABLE_ITEMS[@] TABLE_HEADERS[@]
 
 echo
 echo '###########################'
+echo '> with default separators & padders & paddings & no header:'
 echo
 print_table TABLE_ITEMS[@]
 
 echo
 echo '###########################'
+echo '> with no table (empty):'
 echo
 print_table
 
-#print_table 'yo'
-
-echo
-echo '###########################'
-echo "> demo of a list-like printing:"
+echo '############ DEMO OF print-list:'
 echo
 
 # declare the list arrays
@@ -108,55 +109,60 @@ LIST_VALUES[${#LIST_VALUES[@]}]='my value 2: lorem ipsum dolor site amet'
 # item [0] is padder for separation lines cells
 # item [1] is padder for contents lines cells
 declare -a LIST_PADDERS=()
-LIST_PADDERS[0]="$(printf '%0.1s' "-"{1..120})"
-LIST_PADDERS[1]="$(printf '%0.1s' " "{1..120})"
+LIST_PADDERS[0]="$(printf '%0.1s' "#"{1..120})"
+LIST_PADDERS[1]="$(printf '%0.1s' "_"{1..120})"
 
 # declare the table paddings (number of space(s) from line to cell's contents)
 # item [0] is padding for separation lines cells
 # item [1] is padding for contents lines cells
 declare -a LIST_PADDINGS=()
-LIST_PADDINGS[0]=0
-LIST_PADDINGS[1]=1
+LIST_PADDINGS[0]=2
+LIST_PADDINGS[1]=3
 
 # declare the table separators (characters used to separate cells)
 # item [0] is the separator for separation lines
 # item [1] is the separator for contents lines
 declare -a LIST_SEPARATORS=()
-LIST_SEPARATORS[0]='+'
-LIST_SEPARATORS[1]='|'
+LIST_SEPARATORS[0]='/'
+LIST_SEPARATORS[1]='-'
 
-echo
 echo '###########################'
+echo '> full custom:'
 echo
 print_list LIST_NAMES[@] LIST_VALUES[@] LIST_HEADERS[@] LIST_PADDINGS[@] LIST_PADDERS[@] LIST_SEPARATORS[@]
 
 echo
 echo '###########################'
+echo '> with default separators:'
 echo
 print_list LIST_NAMES[@] LIST_VALUES[@] LIST_HEADERS[@] LIST_PADDINGS[@] LIST_PADDERS[@]
 
 echo
 echo '###########################'
+echo '> with default separators & padders:'
 echo
 print_list LIST_NAMES[@] LIST_VALUES[@] LIST_HEADERS[@] LIST_PADDINGS[@]
 
 echo
 echo '###########################'
+echo '> with default separators & padders & paddings:'
 echo
 print_list LIST_NAMES[@] LIST_VALUES[@] LIST_HEADERS[@]
 
 echo
 echo '###########################'
+echo '> with default separators & padders & paddings & no header:'
 echo
 print_list LIST_NAMES[@] LIST_VALUES[@]
 
 echo
 echo '###########################'
+echo '> with no value items (empty):'
 echo
 print_list LIST_NAMES[@]
 
-echo
 echo '###########################'
+echo '> with nothing at all (empty):'
 echo
 print_list
 
